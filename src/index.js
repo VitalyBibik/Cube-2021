7fcc922008f4511b8d3c7ea2bfd57be6f64792e5
Vitaly Bibik <wital2010@gmail.com>
2021-06-07 11:42:01 +0300

    add render card on server

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const button = document.querySelector('#Load');
+const cardList = document.querySelector('.center__grid')
+
+function renderCard(cardObj) {
+    const card = `<div class="card-wrap">
+                <div class="card">
+                    <img class="card__image" src=${cardObj.url} alt="Футболка">
+                    <h2 class="card__title">
+                        ${cardObj.title}
+                    </h2>
+                    <p class="card__text">
+                        ${cardObj.title}
+                    </p>
+                </div>
+            </div>`
+    cardList.insertAdjacentHTML('beforeend', card)
+    }
+
+function getData() {
+        return fetch(`https://jsonplaceholder.typicode.com/albums/1/photos`)
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+                const json = res.json();
+                return json.then(Promise.reject.bind(Promise))
+            })
+            .catch((err) => {
+                throw err;
+            })
+
+}
+const refresh = () => {
+    getData().then((data) => {
+        return data.forEach((el) => {
+            renderCard(el)
+        })
+    })
+}
+
+refresh()
+button.addEventListener('click', refresh)
+
+
